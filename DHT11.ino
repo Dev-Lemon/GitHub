49cac9e2c21e0b4f403fc59700e3acac6dff1ba8
Dev-Lemon <37819500+Dev-Lemon@users.noreply.github.com>
2018-07-10 01:18:01 +0700

    Add files via upload

---
+++ DHT11.ino
@@ -0,0 +1,130 @@
+#include "DHT.h"
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+
+#define WIFI_AP "YOUR_WIFI_AP"// ten wifi ma node mcu ket noi den
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"// mat khau cua wifi do
+
+#define TOKEN "YOUR $ACCESS_TOKEN"//$ACCESS TOKEN cua device tren Thingsboard cua ban
+
+// DHT
+#define DHTPIN D5//chan ket noi den chan Data cua dht11
+#define DHTTYPE DHT11// kieu cam bien la DHT11
+
+char thingsboardServer[] = "YOUR_THINGSBOARD_HOST_OR_IP";//dia chi ip may chu Thingsboard
+
+WiFiClient wifiClient;
+
+// Initialize DHT sensor.
+DHT dht(DHTPIN, DHTTYPE);
+
+PubSubClient client(wifiClient);
+
+int status = WL_IDLE_STATUS;
+unsigned long lastSend;
+
+void setup(){
+  Serial.begin(115200);
+  dht.begin();
+  delay(10);
+  InitWiFi();
+  client.setServer( thingsboardServer, 1883 );
+  lastSend = 0;
+}
+
+void loop(){
+  if ( !client.connected() ) {
+    reconnect();
+  }
+
+  if ( millis() - lastSend > 1000 ) { // Update and send only after 1 seconds
+    getAndSendTemperatureAndHumidityData();
+    lastSend = millis();
+  }
+
+  client.loop();
+}
+/*ham doc nhhiet do do am tu cam bien va gui len Thingsboard*/
+void getAndSendTemperatureAndHumidityData(){
+  Serial.println("Collecting temperature data.");
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+
+  String temperature = String(t);//ep ve kieu chuoi
+  String humidity = String(h);//ep ve kieu chuoi
+
+
+  // Just debug messages
+  Serial.print( "Sending temperature and humidity : [" );
+  Serial.print( temperature ); Serial.print( "," );
+  Serial.print( humidity );
+  Serial.print( "]   -> " );
+
+  // Prepare a JSON payload string
+  String payload = "{";
+  payload += "\"temperature\":"; payload += temperature; payload += ",";
+  payload += "\"humidity\":"; payload += humidity;
+  payload += "}";
+  //tao 1 chuoi payload la 1 chuoi JSON co dinh dang key-value de chuan bi publish len Thingsboard
+  // Send payload
+  char attributes[100];
+  payload.toCharArray( attributes, 100 );
+  client.publish( "v1/devices/me/telemetry", attributes );
+  Serial.println( attributes );
+  Serial.println(lastSend);
+
+}
+
+void InitWiFi(){
+  Serial.println("Connecting to AP ...");
+  // attempt to connect to WiFi network
+
+  WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to AP");
+}
+
+void reconnect(){
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    status = WiFi.status();
+    if ( status != WL_CONNECTED) {
+      WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+      }
+      Serial.println("Connected to AP");
+    }
+    Serial.print("Connecting to ThingsBoard node ...");
+    // Attempt to connect (clientId, username, password)
+    if ( client.connect("ESP8266 Device", TOKEN, NULL) ) {
+      Serial.println( "[DONE]" );
+    } else {
+      Serial.print( "[FAILED] [ rc = " );
+      Serial.print( client.state() );
+      Serial.println( " : retrying in 5 seconds]" );
+      // Wait 5 seconds before retrying
+      delay( 5000 );
+    }
+  }
+}
